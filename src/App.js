4742f864e70a61f5f0670a215936e0727b21ca31
dhananjay <dhananjaydogne@gmail.com>
2024-01-17 17:44:10 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
+
+import Nav from './Components/Nav';
+import Home from './Pages/Home/Home';
+import Notification from "./Pages/Notification/Notification.jsx";
+import Auth from "./Pages/Authentication/Auth/Auth.jsx";
+import SignInAsPolice from "./Pages/Authentication/signInAsPolice/SignInAsPolice.jsx";
+import SignInAsCameraOwner from "./Pages/Authentication/signInAsCameraOwner/SignInAsCameraOwner.jsx";
+import ResponseForm from "./Pages/Notification/NotiResponse.jsx";
+import MapComponent from "./Pages/Notification/Map.jsx";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+    <Router>
+      <Nav/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/notification" element={<Notification />} />
+        
+        <Route path="/authentication" element={<Auth />} />
+        <Route path="/authentication/police" element={<SignInAsPolice />} />
+        <Route path="/authentication/camera-owner" element={<SignInAsCameraOwner />} />
+        <Route path="/noti-response" element={<ResponseForm />} />
+        <Route path="/livemap" element={<MapComponent />} />
+        {/* <Route path="about" element={<h1>About</h1>} />
+        <Route path="*" element={<h1>Not Found</h1>} /> */}
+      </Routes>
+    </Router>
   );
 }
 
